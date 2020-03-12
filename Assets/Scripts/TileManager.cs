29a1bd00d24f8e9094c11a49d31dfc5325352d11
chasenchamberlain <chasen.chamberlain@gmail.com>
2020-03-12 15:51:37 -0700

    ignore

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TileManager : MonoBehaviour
+{
+    public TextMeshPro debugTxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        debugTxt.text = "6 6\n6 6";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
